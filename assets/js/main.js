3454c8bd236a0d3ebf004195cbe7db4b77f4f133
German Sanchez <germands2013>
2022-06-24 11:15:10 -0300

    si el usuario ingresa null ahora da mensaje de error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-let continuar, random, numero;
+let continuar, random, numero, dificultad;
 while (continuar != "no") {
     do {
         random = parseInt(Math.random() * 10);
         numero = prompt("¿En qué número entre el 0 y el 10 estoy pensando?");
-        if (isNaN(numero)) {
+        if (isNaN(numero) || numero == "" || numero == null) {
             alert("Por favor, ingrese un número válido");
         }
-    } while (isNaN(numero));
+    } while (isNaN(numero) || numero == "" || numero == null);
 
     if (numero != random) {
-        alert(`Casi! Estaba pensando en el ${random}`);
+        alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
     } else {
         alert("Tremendo, adivinaste!");
     }
