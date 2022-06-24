2b5c059c0d140cd771eef1c617bcabf189d2ef5e
German Sanchez <germands2013>
2022-06-24 11:26:14 -0300

    agregados tres niveles de dificultad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,66 @@
 let continuar, random, numero, dificultad;
+// -------While principal------
 while (continuar != "no") {
-    do {
-        random = parseInt(Math.random() * 10);
-        numero = prompt("¿En qué número entre el 0 y el 10 estoy pensando?");
-        if (isNaN(numero) || numero == "" || numero == null) {
-            alert("Por favor, ingrese un número válido");
-        }
-    } while (isNaN(numero) || numero == "" || numero == null);
-
-    if (numero != random) {
-        alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
-    } else {
-        alert("Tremendo, adivinaste!");
+    dificultad = prompt(`Elija la dificultad:\n1.Normal\n2.Difícil\n3.Imposible`);
+    // -------Menu dificultad-----
+    switch (dificultad) {
+        // --------Caso 1--------
+        case "1":
+            do {
+                random = parseInt(Math.random() * 10);
+                numero = prompt("¿En qué número entre el 0 y el 10 estoy pensando?");
+                // ------Valida el número ingresado------
+                if (isNaN(numero) || numero == "" || numero == null) {
+                    alert("Por favor, ingrese un número válido");
+                }
+            } while (isNaN(numero) || numero == "" || numero == null);
+            // -------Resultados-------
+            if (numero != random) {
+                alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
+            } else {
+                alert("Tremendo, adivinaste!");
+            }
+            continuar = prompt(`Desea continuar? (Para salir escriba "no")`);
+            break;
+            // --------Caso 2--------
+        case "2":
+            do {
+                random = parseInt(Math.random() * 100);
+                numero = prompt("¿En qué número entre el 0 y el 100 estoy pensando?");
+                // ------Valida el número ingresado------
+                if (isNaN(numero) || numero == "" || numero == null) {
+                    alert("Por favor, ingrese un número válido");
+                }
+            } while (isNaN(numero) || numero == "" || numero == null);
+            // -------Resultados-------
+            if (numero != random) {
+                alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
+            } else {
+                alert("Buena! Adivinaste, practicás la telekinesis?");
+            }
+            continuar = prompt(`Desea continuar? (Para salir escriba "no")`);
+            break;
+            // --------Caso 3--------
+        case "3":
+            do {
+                random = parseInt(Math.random() * 1000);
+                numero = prompt("¿En qué número entre el 0 y el 1000 estoy pensando?");
+                // ------Valida el número ingresado------
+                if (isNaN(numero) || numero == "" || numero == null) {
+                    alert("Por favor, ingrese un número válido");
+                }
+            } while (isNaN(numero) || numero == "" || numero == null);
+            // -------Resultados-------
+            if (numero != random) {
+                alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
+            } else {
+                alert("Adivinaste, la verdad no entiendo cómo tuviste tanta suerte");
+            }
+            continuar = prompt(`Desea continuar? (Para salir escriba "no")`);
+            break;
+            // --------Caso Default--------
+        default:
+            alert("Por favor, ingrese una dificultad válida");
+            break;
     }
-    continuar = prompt(`Desea continuar? (Para salir escriba "no")`);
 }
