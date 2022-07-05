c5f3563cd4fb1a81dcb4c8a134b8b6152f91fd43
German Sanchez <germands2013>
2022-07-05 16:23:58 -0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,99 @@
-let continuar, random, numero, dificultad;
-// -------While principal------
-while (continuar != "no") {
-    dificultad = prompt(`Elija la dificultad:\n1.Normal\n2.Difícil\n3.Imposible\n4.Salir`);
-    // -------Menu dificultad-----
-    switch (dificultad) {
-        // --------Caso 1--------
-        case "1":
-            do {
-                random = parseInt(Math.random() * 10);
-                numero = prompt("¿En qué número entre el 0 y el 10 estoy pensando?");
-                // ------Valida el número ingresado------
-                if (isNaN(numero) || numero == "" || numero == null) {
-                    alert("Por favor, ingrese un número válido");
-                }
-            } while (isNaN(numero) || numero == "" || numero == null);
-            // -------Resultados-------
-            if (numero != random) {
-                alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
-            } else {
-                alert("Tremendo, adivinaste!");
-            }
-            // continuar = prompt(`Desea continuar? (Para salir escriba "no")`);
-            break;
-            // --------Caso 2--------
-        case "2":
-            do {
-                random = parseInt(Math.random() * 100);
-                numero = prompt("¿En qué número entre el 0 y el 100 estoy pensando?");
-                // ------Valida el número ingresado------
-                if (isNaN(numero) || numero == "" || numero == null) {
-                    alert("Por favor, ingrese un número válido");
-                }
-            } while (isNaN(numero) || numero == "" || numero == null);
-            // -------Resultados-------
-            if (numero != random) {
-                alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
-            } else {
-                alert("Buena! Adivinaste, practicás la telekinesis?");
-            }
-            // continuar = prompt(`Desea continuar? (Para salir escriba "no")`);
-            break;
-            // --------Caso 3--------
-        case "3":
-            do {
-                random = parseInt(Math.random() * 1000);
-                numero = prompt("¿En qué número entre el 0 y el 1000 estoy pensando?");
-                // ------Valida el número ingresado------
-                if (isNaN(numero) || numero == "" || numero == null) {
-                    alert("Por favor, ingrese un número válido");
-                }
-            } while (isNaN(numero) || numero == "" || numero == null);
-            // -------Resultados-------
-            if (numero != random) {
-                alert(`Casi! Elegiste ${numero} y estaba pensando en el ${random}`);
-            } else {
-                alert("Adivinaste, la verdad no entiendo cómo tuviste tanta suerte");
-            }
-            // continuar = prompt(`Desea continuar? (Para salir escriba "no")`);
-            break;
-            // --------Caso 4--------
-        case "4":
-            continuar = "no";
-            break;
-            // --------Caso Default--------
-        default:
-            alert("Por favor, ingrese una dificultad válida");
-            break;
+function comprarProducto(precio) {
+    //Reinicia variable comprar
+    let comprar = "";
+    while (comprar != "no") {
+        let comprando = parseInt(prompt(`Qué producto desea comprar?\n1. Televisión Philco 50" ($80.000)\n2. Notebook Dell Inspiron 3502 ($83.599)\n3. Celular Samsung A51 128Gb ($64.000)\n4. Atrás`));
+        switch (comprando) {
+            case 1:
+                precio = precio + 80000;
+                break;
+            case 2:
+                precio = precio + 83599;
+                break;
+            case 3:
+                precio = precio + 64000;
+                break;
+            case 4:
+                comprar = "no";
+                break;
+            default:
+                alert(`Por favor, ingrese una opción válida`);
+                break;
+        }
+    }
+    return precio;
+}
+
+function calcCuota(precio) {
+    let ingresarBanco, interes, meses, cuota;
+    while (ingresarBanco != "no") {
+        let banco = parseInt(prompt("Elija su banco\n1. Macro\n2. Santander\n3. Superville\n4. Atrás"));
+        switch (banco) {
+            case 1:
+                // Asigna el interés y calcula las cuotas
+                interes = 27;
+                meses = parseInt(prompt("Ingrese la cantidad de cuotas que desea"));
+                cuota = (precio * (1 + interes / 100) / meses).toFixed(2);
+                alert(`Su precio con ${meses} cuotas es de $${cuota}`);
+                break;
+            case 2:
+                interes = 23;
+                meses = parseInt(prompt("Ingrese la cantidad de cuotas que desea"));
+                cuota = (precio * (1 + interes / 100) / meses).toFixed(2);
+                alert(`Su precio con ${meses} cuotas es de $${cuota}`);
+                break;
+            case 3:
+                interes = 20;
+                meses = parseInt(prompt("Ingrese la cantidad de cuotas que desea"));
+                cuota = (precio * (1 + interes / 100) / meses).toFixed(2);
+                alert(`Su precio con ${meses} cuotas es de $${cuota}`);
+                break;
+            case 4:
+                ingresarBanco = "no";
+                break;
+            default:
+                alert(`Por favor, ingrese una opción válida`);
+                break;
+        }
     }
 }
-alert("Gracias por jugar!");
+
+// Función principal
+function ingresarDatos() {
+    let continuar, menuPrincipal, precio = 0;
+    while (continuar != "no") {
+        // Menu principal
+        menuPrincipal = parseInt(prompt("Qué desea hacer?\n1. Comprar\n2. Ver monto (sin cuotas)\n3. Reiniciar monto\n4. Calcular cuotas\n5. Salir"));
+        switch (menuPrincipal) {
+            // Caso 1 - Comprar
+            case 1:
+                precio = comprarProducto(precio);
+                break;
+                // Caso 2 - Ver monto
+            case 2:
+                alert(`El monto es de: $${precio}`);
+                break;
+                // Caso 3 - Reiniciar monto
+            case 3:
+                precio = 0;
+                alert(`El monto ha sido reiniciado`);
+                break;
+                // Caso 4 - Ver y calcular cuotas
+            case 4:
+                precio = calcCuota(precio);
+                break;
+                // Caso 5 - Salir
+            case 5:
+                continuar = "no";
+                break;
+            default:
+                alert(`Por favor, ingrese una opción válida`);
+                break;
+        }
+    }
+
+}
+
+// Ejecución principal
+ingresarDatos();
+document.write(`¡Vuelva pronto!`);
