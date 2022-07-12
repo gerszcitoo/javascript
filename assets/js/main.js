d9e633f65670003f4c0825ed66c4b45dea2f2c18
German Sanchez <germands2013>
2022-07-12 14:06:48 -0300

    Actualización carrito con Arrays

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,40 @@
-function comprarProducto(precio) {
+class Producto {
+    constructor(nombreProd, precioProd) {
+        this.nombreProd = nombreProd;
+        this.precioProd = precioProd;
+    }
+}
+// Array de productos
+let listaProductos = [];
+
+// Función de compra de productos
+function comprarProducto(precioTotal, nombreProd, precioProd) {
     //Reinicia variable comprar
-    let comprar = "";
+    let comprar = "",
+        prod;
     while (comprar != "no") {
         let comprando = parseInt(prompt(`Qué producto desea comprar?\n1. Televisión Philco 50" ($80.000)\n2. Notebook Dell Inspiron 3502 ($83.599)\n3. Celular Samsung A51 128Gb ($64.000)\n4. Atrás`));
         switch (comprando) {
             case 1:
-                precio = precio + 80000;
+                precioTotal = precioTotal + 80000;
+                precioProd = 80000;
+                nombreProd = "Televisión Philco 50";
+                prod = new Producto(nombreProd, precioProd);
+                listaProductos.push(prod);
                 break;
             case 2:
-                precio = precio + 83599;
+                precioTotal = precioTotal + 83599;
+                precioProd = 83599;
+                nombreProd = "Notebook Dell Inspiron 3502";
+                prod = new Producto(nombreProd, precioProd);
+                listaProductos.push(prod);
                 break;
             case 3:
-                precio = precio + 64000;
+                precioTotal = precioTotal + 64000;
+                precioProd = 64000;
+                nombreProd = "Celular Samsung A51 128Gb";
+                prod = new Producto(nombreProd, precioProd);
+                listaProductos.push(prod);
                 break;
             case 4:
                 comprar = "no";
@@ -21,10 +44,10 @@ function comprarProducto(precio) {
                 break;
         }
     }
-    return precio;
+    return precioTotal, nombreProd, precioProd;
 }
 
-function calcCuota(precio) {
+function calcCuota(precioTotal) {
     let ingresarBanco, interes, meses, cuota;
     while (ingresarBanco != "no") {
         let banco = parseInt(prompt("Elija su banco\n1. Macro\n2. Santander\n3. Superville\n4. Atrás"));
@@ -33,19 +56,19 @@ function calcCuota(precio) {
                 // Asigna el interés y calcula las cuotas
                 interes = 27;
                 meses = parseInt(prompt("Ingrese la cantidad de cuotas que desea"));
-                cuota = (precio * (1 + interes / 100) / meses).toFixed(2);
+                cuota = (precioTotal * (1 + interes / 100) / meses).toFixed(2);
                 alert(`Su precio con ${meses} cuotas es de $${cuota}`);
                 break;
             case 2:
                 interes = 23;
                 meses = parseInt(prompt("Ingrese la cantidad de cuotas que desea"));
-                cuota = (precio * (1 + interes / 100) / meses).toFixed(2);
+                cuota = (precioTotal * (1 + interes / 100) / meses).toFixed(2);
                 alert(`Su precio con ${meses} cuotas es de $${cuota}`);
                 break;
             case 3:
                 interes = 20;
                 meses = parseInt(prompt("Ingrese la cantidad de cuotas que desea"));
-                cuota = (precio * (1 + interes / 100) / meses).toFixed(2);
+                cuota = (precioTotal * (1 + interes / 100) / meses).toFixed(2);
                 alert(`Su precio con ${meses} cuotas es de $${cuota}`);
                 break;
             case 4:
@@ -60,30 +83,34 @@ function calcCuota(precio) {
 
 // Función principal
 function ingresarDatos() {
-    let continuar, menuPrincipal, precio = 0;
+    let continuar, menuPrincipal, precioTotal = 0;
     while (continuar != "no") {
         // Menu principal
-        menuPrincipal = parseInt(prompt("Qué desea hacer?\n1. Comprar\n2. Ver monto (sin cuotas)\n3. Reiniciar monto\n4. Calcular cuotas\n5. Salir"));
+        menuPrincipal = parseInt(prompt("Qué desea hacer?\n1. Comprar\n2. Ver monto (sin cuotas)\n3. Reiniciar monto\n4. Calcular cuotas\n5. Ver el carrito (en consola)\n6. Salir"));
         switch (menuPrincipal) {
             // Caso 1 - Comprar
             case 1:
-                precio = comprarProducto(precio);
+                precioTotal = comprarProducto(precioTotal);
                 break;
                 // Caso 2 - Ver monto
             case 2:
-                alert(`El monto es de: $${precio}`);
+                alert(`El monto es de: $${precioTotal}`);
                 break;
                 // Caso 3 - Reiniciar monto
             case 3:
-                precio = 0;
+                precioTotal = 0;
                 alert(`El monto ha sido reiniciado`);
                 break;
                 // Caso 4 - Ver y calcular cuotas
             case 4:
-                calcCuota(precio);
+                calcCuota(precioTotal);
                 break;
-                // Caso 5 - Salir
+                // Caso 5 - Ver carrito con productos
             case 5:
+                console.log(listaProductos);
+                break;
+                // Caso 6 - Salir
+            case 6:
                 continuar = "no";
                 break;
             default:
