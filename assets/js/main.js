69900922918ae0884ba68c410d5f009aa3f93d7b
German Sanchez <germands2013>
2022-07-19 18:33:45 -0300

    agregado saludo dependiendo de horario

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,12 @@ class Producto {
 }
 // Array de productos
 let listaProductos = [];
+// Array de saludos
+let listaBienvenida = ["Buenos días", "Buenas tardes", "Buenas noches"];
+
+/* function saludar() {
+    alert("" + "")
+} */
 
 // Función de compra de productos
 function comprarProducto(precioTotal, nombreProd, precioProd) {
@@ -91,62 +97,76 @@ function calcCuota(precioTotal) {
 // Función principal
 function ingresarDatos() {
     let continuar, menuPrincipal, busquedaUsuario, precioTotal = 0;
-    while (continuar != "no") {
-        // Menu principal
-        menuPrincipal = parseInt(prompt("Qué desea hacer?\n1. Comprar\n2. Ver el carrito\n3. Vaciar carrito\n4. Buscar\n5. Ver monto (sin cuotas)\n6. Calcular cuotas\n7. Salir"));
-        switch (menuPrincipal) {
-            // Caso 1 - Comprar
-            case 1:
-                precioTotal = comprarProducto(precioTotal);
-                precioTotal = listaProductos.reduce((acumulador, elemento) => acumulador + elemento.precioProd, 0);
-                break;
-                // Caso 2 - Ver carrito con productos
-            case 2:
-                let carrito = [];
-                if (listaProductos == "") {
-                    alert("El carrito está vacío");
-                } else {
-                    for (const producto of listaProductos) {
-                        carrito.push(producto.nombreProd);
-                    }
-                    alert(carrito.join("\n"));
-                }
-                break;
-                // Caso 3 - Vaciar carrito
-            case 3:
-                listaProductos = [];
-                precioTotal = 0;
-                alert(`El carrito ha sido vaciado`);
-                break;
-                // Caso 4 - Buscar
-            case 4:
-                busquedaUsuario = prompt("Qué producto desea buscar?");
-                busquedaUsuario.toUpperCase();
-                let buscar = listaProductos.find(producto => producto.nombreProd == busquedaUsuario);
-                if (buscar == null) {
-                    alert(`No se encontró ningún producto que coincida con "${busquedaUsuario}"`);
-                } else {
-                    alert(buscar);
-                }
-                break;
-                // Caso 5 - Ver monto
-            case 5:
-                alert(`El monto es de: $${precioTotal}`);
-                break;
-                // Caso 6 - Ver y calcular cuotas
-            case 6:
-                calcCuota(precioTotal);
-                break;
-                // Caso 7 - Salir
-            case 7:
-                continuar = "no";
-                break;
-            default:
-                alert(`Por favor, ingrese una opción válida`);
-                break;
-        }
+    // Saludo según horario
+    var hora = new Date().getHours();
+    if (hora >= 5 && hora <= 12) {
+        saludarHorario = listaBienvenida[0];
+    } else if (hora > 12 && hora <= 19) {
+        saludarHorario = listaBienvenida[1];
+    } else {
+        saludarHorario = listaBienvenida[2];
     }
+    // Pregunta si desea ver el prompt de compra
+    if (confirm(`${saludarHorario}\n¿Desea comprar algún producto?`)) {
+        while (continuar != "no") {
+            // Menu principal
+            menuPrincipal = parseInt(prompt("Qué desea hacer?\n1. Comprar\n2. Ver el carrito\n3. Vaciar carrito\n4. Buscar\n5. Ver monto (sin cuotas)\n6. Calcular cuotas\n7. Salir"));
+            switch (menuPrincipal) {
+                // Caso 1 - Comprar
+                case 1:
+                    precioTotal = comprarProducto(precioTotal);
+                    precioTotal = listaProductos.reduce((acumulador, elemento) => acumulador + elemento.precioProd, 0);
+                    break;
+                    // Caso 2 - Ver carrito con productos
+                case 2:
+                    let carrito = [];
+                    if (listaProductos == "") {
+                        alert("El carrito está vacío");
+                    } else {
+                        for (const producto of listaProductos) {
+                            carrito.push(producto.nombreProd);
+                        }
+                        alert(carrito.join("\n"));
+                    }
+                    break;
+                    // Caso 3 - Vaciar carrito
+                case 3:
+                    listaProductos = [];
+                    precioTotal = 0;
+                    alert(`El carrito ha sido vaciado`);
+                    break;
+                    // Caso 4 - Buscar
+                case 4:
+                    busquedaUsuario = prompt("Qué producto desea buscar?");
+                    busquedaUsuario.toUpperCase();
+                    let buscar = listaProductos.find(producto => producto.nombreProd == busquedaUsuario);
+                    if (buscar == null) {
+                        alert(`No se encontró ningún producto que coincida con "${busquedaUsuario}"`);
+                    } else {
+                        alert(buscar);
+                    }
+                    break;
+                    // Caso 5 - Ver monto
+                case 5:
+                    alert(`El monto es de: $${precioTotal}`);
+                    break;
+                    // Caso 6 - Ver y calcular cuotas
+                case 6:
+                    calcCuota(precioTotal);
+                    break;
+                    // Caso 7 - Salir
+                case 7:
+                    continuar = "no";
+                    break;
+                default:
+                    alert(`Por favor, ingrese una opción válida`);
+                    break;
+            }
+        }
 
+    } else {
+        continuar = "no";
+    }
 }
 
 // Ejecución principal
